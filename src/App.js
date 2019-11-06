177d73636c24728f7ab4cd92f7e3816b30d932e0
Sion GENDERS <sion.genders@estiam.com>
2019-11-06 12:18:09 +0100

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import List from './Components/List';
+import Add from './Components/Add';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Route exact path='/' component={List} />
+        <Route exact path='/Add' component={Add} />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Components/Add.js
@@ -0,0 +1,125 @@
+import React, { Component } from 'react'
+
+
+class Add extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: '',
+      type: '',
+      captured: false,
+      description: '',
+      attacks: '',
+      photo: '',
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleCheckbox = this.handleCheckbox.bind(this);
+    this.handleFile = this.handleFile.bind(this);
+  }
+
+  handleChange(element) {
+    const inputName = element.target.name;
+    const inputValue = element.target.value;
+
+    const statusCopy = Object.assign({}, this.state);
+    statusCopy[inputName] = inputValue;
+
+    console.log(statusCopy);
+    this.setState(statusCopy);
+  }
+
+  handleCheckbox(element) {
+    const inputName = element.target.name;
+    const inputValue = element.target.checked;
+
+    const statusCopy = Object.assign({}, this.state);
+    statusCopy[inputName] = inputValue;
+
+    console.log(statusCopy);
+    this.setState(statusCopy);
+  }
+
+  handleFile(element) {
+    const inputName = element.target.name;
+    const inputFile = element.target.files[0];
+    const statusCopy = Object.assign({}, this.state);
+
+    if (inputFile != null) {
+      const fileReader = new FileReader();
+
+      fileReader.onload = (fle) => {
+        const file = fle.target.result;
+        console.log(file);
+
+        statusCopy[inputName] = file;
+        console.log(statusCopy);
+
+        this.setState(statusCopy);
+      };
+
+      fileReader.readAsDataURL(inputFile);
+    }
+  }
+
+
+  render() {
+    return (
+      <div>
+        <h1>Ajouter un pokémon</h1>
+        <form>
+          <div>
+            <label htmlFor='name'>Nom</label>
+            <input
+              value={this.state.name}
+              onChange={this.handleChange}
+              name='name'
+              id='name'
+            ></input>
+          </div>
+          <div>
+            <label htmlFor='type'>Type</label>
+            <input
+              value={this.state.type}
+              onChange={this.handleChange}
+              name='type'
+              id='type'></input>
+          </div>
+          <div>
+            <label htmlFor='description'>Description</label>
+            <textarea
+              value={this.state.description}
+              onChange={this.handleChange}
+              name='description'
+              id='description'></textarea>
+          </div>
+          <div>
+            <label htmlFor='attacks'>Attaques</label>
+            <textarea
+              value={this.state.attacks}
+              onChange={this.handleChange}
+              name='attacks' id='attacks'></textarea>
+          </div>
+          <div>
+            <label htmlFor='captured'>Capturé</label>
+            <input
+              checked={this.state.captured}
+              onChange={this.handleCheckbox}
+              name='captured' id='captured' type='checkbox'></input>
+          </div>
+          <div>
+            <label htmlFor='photo'>Photo</label>
+            <input
+              onChange={this.handleFile}
+              name='photo' id='photo' type='file'></input>
+          </div>
+          <div>
+            <button>GO !</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Add;
